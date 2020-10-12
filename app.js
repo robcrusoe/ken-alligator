9353fcc47dd7e81effd9558de95a75176cbc309e
robcrusoe <arka.sain@aol.com>
2020-10-13 02:21:37 +0530

    Creates GET|POST|PUT|DELETE routes

--- app.js
+++ app.js
@@ -4,6 +4,26 @@ import data from './data/data.json';
 const app = express();
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    return res.send(`A get request with / route on port ${PORT}`);
+});
+
+
+app.post('/newItem', (req, res) => {
+    return res.send(`A post request with /newItem route on port ${PORT}`);
+});
+
+
+app.put('/item', (req, res) => {
+    return res.send(`A put request with /item route on port ${PORT}`);
+});
+
+
+app.delete('/item', (req, res) => {
+    return res.send(`A delete request with /item route on port ${PORT}`);
+});
+
+
 app.listen(PORT, () => {
     console.log(data);
     console.log(`Your server is running on PORT ${PORT}`);
