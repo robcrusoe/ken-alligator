5b55187e0873ea9a41e624015684fc8d3311e316
robcrusoe <arka.sain@aol.com>
2020-10-12 21:43:04 +0530

    Adds data to and sets up the server at 3000

--- app.js
+++ app.js
@@ -1,20 +1,10 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+import express from 'express';
+import data from './data/data.json';
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
+const PORT = 3000;
 
-var app = express();
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-module.exports = app;
+app.listen(PORT, () => {
+    console.log(data);
+    console.log(`Your server is running on PORT ${PORT}`);
+});
